ad1cde44c5b38bf28625bc801337e20fa3af6284
Hallvard Lavik <hallvardlavik@gmail.com>
2026-05-14 14:01:02 +0200

    Proper path?

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import { Elysia } from "elysia";
 import { staticPlugin } from "@elysiajs/static";
+import { dirname, join } from "path";
 
-const reinli = new Elysia().use(staticPlugin({ assets: "dist", prefix: "/" }));
+let __dirname = dirname(new URL(import.meta.url).pathname);
+__dirname =
+  __dirname.startsWith("/") && __dirname.includes(":")
+    ? __dirname.replace(/^\/([A-Z]):/, "$1:\\").replace(/\//g, "\\")
+    : __dirname;
+
+const reinli = new Elysia().use(staticPlugin({ assets: join(__dirname, "dist"), prefix: "/" }));
 
 export default reinli;
 
